a22aad0c98dd8e6ac19f91705992d78d0d4b6fda
Bory <aaauzbek@gmail.com>
2025-01-12 01:10:18 -0600

    polishing up everything V1

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -188,7 +188,7 @@ public final class HackList implements UpdateListener
 	public final VeinMinerHack veinMinerHack = new VeinMinerHack();
 	public final XRayHack xRayHack = new XRayHack();
 	
-	public final BoryTestHack boryTestHack = new BoryTestHack();
+	public final MineBoxAutoMineHack mineBoxAutoMineHack = new MineBoxAutoMineHack();
 	
 	private final TreeMap<String, Hack> hax =
 		new TreeMap<>(String::compareToIgnoreCase);
--- src/main/java/net/wurstclient/hacks/MineBoxAutoMineHack.java
+++ src/main/java/net/wurstclient/hacks/MineBoxAutoMineHack.java
@@ -1,6 +1,5 @@
 package net.wurstclient.hacks;
 
-import java.lang.reflect.Field;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -11,24 +10,24 @@ import org.lwjgl.opengl.GL11;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.ShaderProgramKeys;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.particle.ParticleTextureSheet;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.InteractionEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.decoration.DisplayEntity.TextDisplayEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3i;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
-import net.wurstclient.WurstClient;
 import net.wurstclient.ai.PathFinder;
 import net.wurstclient.ai.PathProcessor;
 import net.wurstclient.commands.PathCmd;
@@ -36,43 +35,49 @@ import net.wurstclient.events.HandleInputListener;
 import net.wurstclient.events.RenderListener;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
-import net.wurstclient.mixinterface.IKeyBinding;
 import net.wurstclient.settings.AttackSpeedSliderSetting;
 import net.wurstclient.settings.CheckboxSetting;
-import net.wurstclient.settings.EspBoxSizeSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.settings.SwingHandSetting;
 import net.wurstclient.settings.SwingHandSetting.SwingHand;
-import net.wurstclient.util.BlockPlacer;
-import net.wurstclient.util.BlockPlacer.BlockPlacingParams;
+import net.wurstclient.settings.filterlists.EntityFilterList;
+import net.wurstclient.settings.filters.FilterInvisibleSetting;
+import net.wurstclient.settings.filters.FilterSleepingSetting;
 import net.wurstclient.util.EntityUtils;
+import net.wurstclient.util.FakePlayerEntity;
 import net.wurstclient.util.RegionPos;
 import net.wurstclient.util.RenderUtils;
 
-//TODO The display names sometimes have custom things in them we want the Mincraft account name
-
-@SearchTags({"bory test", "BoryTest", "bory tests"})
-public final class BoryTestHack extends Hack
+@SearchTags({"MineBox", "mine box", "MineBoxAutoMine", "mine box auto mine"})
+public final class MineBoxAutoMineHack extends Hack
 	implements UpdateListener, RenderListener, HandleInputListener
 {
 	
 	private MineBoxEntity currentTarget;
-	
 	private MineBoxEntityFinder entityFinder;
 	private MineBoxPathFinder pathFinder;
 	
-	private final SliderSetting fov = new SliderSetting("FOV", "Field of View",
-		360, 30, 360, 10, ValueDisplay.DEGREES);
-	private final SliderSetting rotationSpeed =
-		new SliderSetting("Rotation Speed", 600, 10, 3600, 10,
-			ValueDisplay.DEGREES.withSuffix("/s"));
 	private final CheckboxSetting damageIndicator =
 		new CheckboxSetting("Damage Indicator", true);
-	private final EspBoxSizeSetting boxSize =
-		new EspBoxSizeSetting("Box Size Mode");
+
 	private final SwingHandSetting swingHand = new SwingHandSetting(
 		SwingHandSetting.genericCombatDescription(this), SwingHand.CLIENT);
+	
+	private final SliderSetting range = new SliderSetting("Range",
+		"Determines how far Killaura will reach to attack entities.\n"
+			+ "Anything that is further away than the specified value will not be attacked.",
+		3, 1, 10, 0.05, ValueDisplay.DECIMAL);
+	
+	private final SliderSetting peopleSearchRange = new SliderSetting(
+		"Range To search for people",
+		"Determines how far to search for people to pause the hack.\n"
+			+ "Anything that is further away than the specified value will not be considered.",
+		25, 1, 100, 1, ValueDisplay.DECIMAL);
+	
+	private final SliderSetting entitySearchRange =
+		new SliderSetting("How far to search for minable entities", 25, 1, 100,
+			1, ValueDisplay.DECIMAL);
 	
 	private final AttackSpeedSliderSetting speed =
 		new AttackSpeedSliderSetting();
@@ -86,17 +91,21 @@ public final class BoryTestHack extends Hack
 			100, 0, 1000, 50, ValueDisplay.INTEGER.withPrefix("\u00b1")
 				.withSuffix("ms").withLabel(0, "off"));
 	
-	public BoryTestHack()
+	private final EntityFilterList entityFilters = new EntityFilterList(
+		new FilterSleepingSetting("Won't pause for sleeping players.", false),
+		new FilterInvisibleSetting("Won't pause invisible players.", false));
+	
+	public MineBoxAutoMineHack()
 	{
-		super("BoryTest");
+		super("MineBoxAutoMine");
 		setCategory(Category.FUN);
-		addSetting(fov);
-		addSetting(rotationSpeed);
 		addSetting(damageIndicator);
-		addSetting(boxSize);
 		addSetting(speed);
 		addSetting(speedRandMS);
 		addSetting(swingHand);
+		addSetting(range);
+		addSetting(peopleSearchRange);
+		addSetting(entitySearchRange);
 	}
 	
 	@Override
@@ -125,6 +134,26 @@ public final class BoryTestHack extends Hack
 	@Override
 	public void onUpdate()
 	{
+		speed.updateTimer();
+		
+		PlayerEntity player = MC.player;
+		ClientWorld world = MC.world;
+		Stream<AbstractClientPlayerEntity> stream = world.getPlayers()
+			.parallelStream().filter(e -> !e.isRemoved() && e.getHealth() > 0)
+			.filter(e -> e != player)
+			.filter(e -> !(e instanceof FakePlayerEntity))
+			.filter(e -> Math.abs(e.getY() - MC.player.getY()) <= 1e6)
+			.filter(e -> MC.player.squaredDistanceTo(e) <= peopleSearchRange
+				.getValueSq());
+		
+		stream = entityFilters.applyTo(stream);
+		
+		// Pause hack when player detected
+		if(!(stream.collect(Collectors.toList()).isEmpty()))
+		{
+			PathProcessor.releaseControls();
+			return;
+		}
 		
 		if(currentTarget == null)
 		{
@@ -154,8 +183,7 @@ public final class BoryTestHack extends Hack
 				currentTarget.getPos(), tempCurrentClosestTarget.getPos()))
 			{
 				currentTarget.lookAt();
-				IKeyBinding.get(MC.options.attackKey).simulatePress(true);
-				IKeyBinding.get(MC.options.attackKey).simulatePress(false);
+				currentTarget.setHitBox();
 				
 				return;
 			}
@@ -163,15 +191,10 @@ public final class BoryTestHack extends Hack
 		
 		if(isCurentTarget)
 		{
-			Vec3d targetSpot = ParticleUtils.findReachableParticle();
+			currentTarget.setHitBox();
 			
-			if(targetSpot != null)
+			if(currentTarget.getHitBox() == null)
 			{
-				currentTarget.setHitBox(targetSpot);
-				// faceAndAttack(targetSpot);
-			}else
-			{
-				currentTarget.setHitBox(null);
 				pathFinder.reSet();
 			}
 			
@@ -252,92 +275,19 @@ public final class BoryTestHack extends Hack
 	@Override
 	public void onHandleInput()
 	{
-		// TODO After hitting once wait untill target is not the same
 		if(currentTarget == null)
 			return;
 		if(currentTarget.getHitBox() == null)
+			return;
+		if(!speed.isTimeToAttack())
 			return;
 		
 		MC.interactionManager.attackEntity(MC.player,
 			currentTarget.getHitBox());
 		swingHand.swing(Hand.MAIN_HAND);
 		
-		currentTarget.setHitBox(null);
+		currentTarget.clearHitBox();
 		speed.resetTimer(speedRandMS.getValue());
-	}
-	
-	// Utility class for particle handling
-	public static class ParticleUtils
-	{
-		
-		public static Vec3d findReachableParticle()
-		{
-			ParticleManager particleManager =
-				MinecraftClient.getInstance().particleManager;
-			List<Vec3d> points = new ArrayList<>();
-			
-			try
-			{
-				Field particlesField =
-					ParticleManager.class.getDeclaredField("particles");
-				particlesField.setAccessible(true);
-				
-				@SuppressWarnings("unchecked")
-				Map<ParticleTextureSheet, Queue<Particle>> particlesMap =
-					(Map<ParticleTextureSheet, Queue<Particle>>)particlesField
-						.get(particleManager);
-				
-				for(Queue<Particle> particleQueue : particlesMap.values())
-				{
-					for(Particle particle : particleQueue)
-					{
-						if(isGlowParticle(particle)
-							&& isParticleReachable(particle))
-						{
-							points.add(particle.getBoundingBox().getCenter());
-						}
-					}
-				}
-				
-				if(!points.isEmpty())
-				{
-					return findCenter(points);
-				}
-			}catch(NoSuchFieldException | IllegalAccessException e)
-			{
-				e.printStackTrace();
-			}
-			
-			return null;
-		}
-		
-		private static boolean isGlowParticle(Particle particle)
-		{
-			return particle.getClass().getSimpleName().contains("GlowParticle");
-		}
-		
-		private static boolean isParticleReachable(Particle particle)
-		{
-			Vec3d particlePos = particle.getBoundingBox().getCenter();
-			BlockPos pos = new BlockPos((int)particlePos.x, (int)particlePos.y,
-				(int)particlePos.z);
-			BlockPlacingParams params = BlockPlacer.getBlockPlacingParams(pos);
-			double range = 4; // 1 block range
-			return params != null && params.distanceSq() <= range;
-		}
-		
-		private static Vec3d findCenter(List<Vec3d> points)
-		{
-			double sumX = 0, sumY = 0, sumZ = 0;
-			for(Vec3d point : points)
-			{
-				sumX += point.x;
-				sumY += point.y;
-				sumZ += point.z;
-			}
-			return new Vec3d(sumX / points.size(), sumY / points.size(),
-				sumZ / points.size());
-		}
 	}
 	
 	public class MineBoxEntity
@@ -396,15 +346,27 @@ public final class BoryTestHack extends Hack
 		
 		public void lookAt()
 		{
-			MC.player.setYaw(this.facing.asRotation());
+			if(this.facing.asRotation() != MC.player.getYaw())
+			{
+				MC.player.setYaw(this.facing.asRotation());
+			}
 		}
 		
 		public BlockPos getPos()
 		{
-			Vec3i tempI = new Vec3i(pos.getX(), pos.getY(), pos.getZ());
-			Vec3d tempD = new Vec3d(tempI);
+			Block block = MC.world.getBlockState(pos).getBlock();
+			int y = pos.getY();
+			int x = pos.getX();
+			int z = pos.getZ();
+			// Loop blocks downwards (in Y) untill solid block is found
+			while(block instanceof AirBlock)
+			{
+				block =
+					MC.world.getBlockState(new BlockPos(x, y, z)).getBlock();
+				y--;
+			}
 			
-			return BlockPos.ofFloored(tempD);
+			return new BlockPos(x, y + 2, z);
 		}
 		
 		public boolean isHarvestable()
@@ -417,53 +379,37 @@ public final class BoryTestHack extends Hack
 			return hitBox;
 		}
 		
-		public void setHitBox(Vec3d hitBox)
+		public void clearHitBox()
 		{
-			if(hitBox == null)
-			{
-				this.hitBox = null;
-				return;
-			}
+			this.hitBox = null;
+		}
+		
+		public void setHitBox()
+		{
+			double rangeSq = range.getValueSq();
+			Entity smallest = null;
+			double smallestSize = 0;
 			
 			Stream<Entity> stream =
 				StreamSupport.stream(MC.world.getEntities().spliterator(), true)
-					.filter(IS_VALID_INTERACT_ENTITY);
-			
-			List<Entity> closest = new ArrayList<>();
+					.filter(IS_VALID_INTERACT_ENTITY)
+					.filter(e -> MC.player.squaredDistanceTo(e) <= rangeSq);
+			;
 			
 			for(Entity e : stream.collect(Collectors.toList()))
 			{
 				if(e instanceof InteractionEntity)
 				{
-					InteractionEntity entity = (InteractionEntity)e;
-					if(entity.getBlockPos().getSquaredDistance(hitBox) < 1)
-					{
-						closest.add(entity);
-					}
-					
-				}
-			}
-			
-			Entity smallest = null;
-			double smallestSize = 0;
-			
-			if(closest.size() > 0)
-			{
-				for(Entity i : closest)
-				{
 					if(smallestSize == 0)
 					{
-						smallest = i;
-						smallestSize = i.getBoundingBox().getLengthX();
-					}else if(i.getBoundingBox().getLengthX() < smallestSize)
+						smallest = e;
+						smallestSize = e.getBoundingBox().getLengthX();
+					}else if(e.getBoundingBox().getLengthX() < smallestSize)
 					{
-						smallest = i;
-						smallestSize = i.getBoundingBox().getLengthX();
+						smallest = e;
+						smallestSize = e.getBoundingBox().getLengthX();
 					}
 				}
-			}else
-			{
-				smallest = null;
 			}
 			
 			this.hitBox = smallest;
@@ -471,10 +417,6 @@ public final class BoryTestHack extends Hack
 		
 		public boolean matchesPlayerName()
 		{
-			System.out.println(MC.player.getName().getString());
-			System.out.println(MC.player.getDisplayName().getString());
-
-
 			return entity.getText().getString()
 				.contains(MC.player.getDisplayName().getString());
 		}
@@ -498,6 +440,8 @@ public final class BoryTestHack extends Hack
 		{
 			Stream<Entity> stream =
 				StreamSupport.stream(MC.world.getEntities().spliterator(), true)
+					.filter(e -> MC.player
+						.squaredDistanceTo(e) <= entitySearchRange.getValueSq())
 					.filter(IS_VALID_TEXT_ENTITY);
 			
 			targets.clear();
